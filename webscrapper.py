f25d481f36907c583d9f9b0f528e025722b7ee1b
Sulav306 <sulavshrestha315@proton.me>
2025-08-03 19:44:46 +0545

    Printing every content where a tag is included.

--- webscrapper.py
+++ webscrapper.py
@@ -4,3 +4,6 @@ url="https://pixelford.com/blog/"
 response=requests.get(url,headers={'user-agent':'Hello'}) #changing user agent because the website got too many requests from python header leading it to block every requests which had the name python as its header
 html=response.content
 soup=BeautifulSoup(html,'html.parser')
+a_tags=soup.find_all('a',class_="entry-title-link")
+for a in a_tags:
+    print(a)
