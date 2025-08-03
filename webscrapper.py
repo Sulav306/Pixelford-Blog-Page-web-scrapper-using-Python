bb21b2bcae77e9fd2a30f402832cc899b513f3a5
Sulav306 <sulavshrestha315@proton.me>
2025-08-03 19:31:04 +0545

    importing beautiful soup which converts messy html into an organized way or finds the required thing

--- webscrapper.py
+++ webscrapper.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
 url="https://pixelford.com/blog/"
-response=requests.get(url,headers={'user-agent':'Hello'})
-print(response.content)
+response=requests.get(url,headers={'user-agent':'Hello'}) #changing user agent because the website got too many requests from python header leading it to block every requests which had the name python as its header
+html=response.content
+soup=BeautifulSoup(html,'html.parser')
